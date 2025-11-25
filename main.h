8b2c39f9ba871f3828605cd2e9ab59f337f711f7
Crypoune <a.messenet@gmail.com>
2025-11-25 19:59:25 +0100

    add prototypes and includes

--- main.h
+++ main.h
@@ -1,12 +1,38 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
 
+/**
+ * struct type_format - defines a structure for format specifiers
+ * and their associated functions
+ *
+ * @e: The format specifier (e.g., 'c', 's', '%')
+ * @f: The function associated with the specifier
+ */
+typedef struct type_format
+{
+	char	e;
+	int		(*f)(va_list a);
+} f_t;
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 int _printf(const char *format, ...);
-int print_s(const char *str);
-int print_c(const char c);
+int	(*print_format(const char *format))(va_list);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_integer(va_list args);
+int print_percent(va_list args);
 
 #endif
