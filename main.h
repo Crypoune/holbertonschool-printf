4ff09137e29ef13131eda0c676c6129b64c27ac9
Crypoune <a.messenet@gmail.com>
2025-11-25 10:00:59 +0100

    feat: add prototypes str and c + includes

--- main.h
+++ main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 int _printf(const char *format, ...);
 int print_s(const char *str);
 int print_c(const char c);
+
+#endif
